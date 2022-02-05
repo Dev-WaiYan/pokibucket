1ab5695bc8efdd80e29defd33a0b03e42d6f105d
Dev-WaiYan <htetwaiwilsonsprite@gmail.com>
2022-02-06 02:31:11 +0630

    [ Add: Fontawesome ]; [ Add: Loadmore and its feature ]; [ Development warning message added in apiConfig.ts ]

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
+import Button from "@/components/Button/Button";
 import ItemCard from "@/components/Card/ItemCard/ItemCard";
 import Layout from "@/components/Layout/Layout";
 import Card from "models/Card";
+import { useState } from "react";
 import { getCards } from "services/cardService";
+import "@fortawesome/fontawesome-free/css/all.css";
+import styles from "./home.module.css";
 
 export async function getStaticProps() {
   return {
@@ -16,7 +20,22 @@ interface Props {
   cards: Card[];
 }
 
-export default function Home({ cards }: Props) {
+export default function Home(props: Props) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [cards, setCards] = useState(props.cards);
+
+  // Handlers - Start
+  const showMore = () => {
+    setIsLoading(true);
+    getCards(currentPage + 1).then((nextCards) => {
+      setCurrentPage(currentPage + 1);
+      setCards([...cards, ...nextCards]);
+      setIsLoading(false);
+    });
+  };
+  // Handlers - End
+
   return (
     <Layout>
       <div className="container">
@@ -35,6 +54,14 @@ export default function Home({ cards }: Props) {
             </div>
           ))}
         </div>
+      </div>
+      <div className="d-flex align-items-center flex-column my-4">
+        {isLoading && <b>Please Wait...</b>}
+        <Button
+          title={<i className="fas fa-search">&nbsp;&nbsp;show more</i>}
+          onClick={showMore}
+          className={styles.btnShowMore}
+        />
       </div>
     </Layout>
   );
--- services/apiConfig.ts
+++ services/apiConfig.ts
@@ -4,8 +4,10 @@ export const getApiInstance = (): Promise<AxiosInstance> => {
   return new Promise((resolve) => {
     resolve(
       axios.create({
-        baseURL: process.env.API_SERVER_URL,
+        baseURL: process.env.NEXT_PUBLIC_API_SERVER_URL,
         headers: {
+          // Client side api call like load more will not include API_KEY.
+          // Because API_KEY is secret and only used in server side.
           "X-Api-Key": process.env.API_KEY ? process.env.API_KEY : "",
         },
       })
